6f9802c25e96bb8a20254fe47020b7acbac41b3c
aminkhalaj73 <mohammad.a.khalaj@gmail.com>
2018-12-09 09:43:05 +0330

    3th chapter before quiz 2

---
+++ web.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*Route::get('/', function () {
+    return view('welcome');
+});*/
+
+ Route::get('/' , 'pagecontroller@index');
+
+ Route::get('/product' , function() {
+     $product = DB::table('product')
+       /*  ->where('name' , 'like' , '%9%')
+         ->where('description' , 'like' , '%smart%')*/
+         ->get();
+         return view('product.index' , compact('product'));
+ });
+
+ Route::get('/product/{id}' , function($id) {
+     $product = DB::table('product')
+         ->where('id' , '=' , $id)
+         ->first();
+     return view('product.show' , compact('product'));
+ });
